c4619c97ff687c09538f6ab0288cab89d2cfae5c
Miko Chu <miko.codecrafters@gmail.com>
2024-11-02 16:38:00 +0800

    add check board functions to see winners

---
+++ src/check_board.h
@@ -0,0 +1,88 @@
+int check_board_horizontal (int board[3][3])
+{
+  int game_over = 0;
+  for (int row = 0; row < 3 && !game_over; row++) {
+    int player_one_points = 0;
+    for (int col = 0; col < 3; col++) {
+      if (board[row][col] == 1) {
+        player_one_points++;
+      } else if (board[row][col] == 2) {
+        player_one_points--;
+      }
+
+      if (player_one_points >= 3) {
+        printf("Player 1 wins!\n");
+        game_over = 1;
+        return 1;
+      } else if (player_one_points <= -3) {
+        printf("Player 2 wins!\n");
+        game_over = 1;
+        return 2;
+      }
+    }
+    printf("points %d: %d\n", row, player_one_points);
+  }
+
+  return 0;
+}
+
+int check_board_vertical (int board[3][3])
+{
+  int game_over = 0;
+  for (int col = 0; col < 3 && !game_over; col++) {
+    int player_one_points = 0;
+    for (int row = 0; row < 3; row++) {
+      // printf("%d ", col);
+      // printf("%d, ", board[row][col]);
+      if (board[row][col] == 1) {
+        player_one_points++;
+      } else if (board[row][col] == 2) {
+        player_one_points--;
+      }
+
+      if (player_one_points >= 3) {
+        printf("Player 1 wins!\n");
+        game_over = 1;
+        return 1;
+      } else if (player_one_points <= -3) {
+        printf("Player 2 wins!\n");
+        game_over = 1;
+        return 0;
+      }
+    }
+  }
+  return 0;
+}
+
+int check_broad_cross (int board[3][3])
+{
+  int player_x_one_points = 0;
+  int player_x_r_one_points = 0;
+  int ix = 2;
+  for (int i = 0; i < 3; i++) {
+    if (board[i][i] == 1) {
+      player_x_one_points++;
+    } else if (board[i][i] == 2) {
+      player_x_one_points--;
+    }
+
+    printf("%d, ", ix);
+    if (board[i][ix] == 1) {
+      player_x_r_one_points++;
+    } else if (board[i][ix] == 2) {
+      player_x_r_one_points--;
+    }
+
+    ix--;
+
+    if (player_x_one_points >= 3 || player_x_r_one_points >= 3) {
+      printf("Player 1 wins!\n");
+      return 1;
+    } else if (player_x_one_points <= -3 || player_x_r_one_points <= -3) {
+      printf("Player 2 wins!\n");
+      return 2;
+    }
+  }
+
+  return 0;
+}
