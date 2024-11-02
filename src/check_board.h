ba9276608af17cc8167e72c1976fbb024fb4bc15
Miko Chu <miko.codecrafters@gmail.com>
2024-11-02 17:10:40 +0800

    fix formatting

--- src/check_board.h
+++ src/check_board.h
@@ -1,19 +1,24 @@
-void reset_board (int board[3][3])
+void reset_board(int board[3][3])
 {
   int checked = 0;
-  for (int row = 0; row < 3; row++) {
-    for (int col = 0; col < 3; col++) {
+  for (int row = 0; row < 3; row++)
+  {
+    for (int col = 0; col < 3; col++)
+    {
       board[row][col] = 0;
     }
   }
 }
 
-int check_board_draw (int board[3][3])
+int check_board_draw(int board[3][3])
 {
   int checked = 0;
-  for (int row = 0; row < 3; row++) {
-    for (int col = 0; col < 3; col++) {
-      if (board[row][col] != 0) {
+  for (int row = 0; row < 3; row++)
+  {
+    for (int col = 0; col < 3; col++)
+    {
+      if (board[row][col] != 0)
+      {
         checked++;
       }
     }
@@ -22,23 +27,31 @@ int check_board_draw (int board[3][3])
   return checked == 9;
 }
 
-int check_board_horizontal (int board[3][3])
+int check_board_horizontal(int board[3][3])
 {
   int game_over = 0;
-  for (int row = 0; row < 3 && !game_over; row++) {
+  for (int row = 0; row < 3 && !game_over; row++)
+  {
     int player_one_points = 0;
-    for (int col = 0; col < 3; col++) {
-      if (board[row][col] == 1) {
+    for (int col = 0; col < 3; col++)
+    {
+      if (board[row][col] == 1)
+      {
         player_one_points++;
-      } else if (board[row][col] == 2) {
+      }
+      else if (board[row][col] == 2)
+      {
         player_one_points--;
       }
 
-      if (player_one_points >= 3) {
+      if (player_one_points >= 3)
+      {
         printf("Player 1 wins!\n");
         game_over = 1;
         return 1;
-      } else if (player_one_points <= -3) {
+      }
+      else if (player_one_points <= -3)
+      {
         printf("Player 2 wins!\n");
         game_over = 1;
         return 2;
@@ -50,25 +63,33 @@ int check_board_horizontal (int board[3][3])
   return 0;
 }
 
-int check_board_vertical (int board[3][3])
+int check_board_vertical(int board[3][3])
 {
   int game_over = 0;
-  for (int col = 0; col < 3 && !game_over; col++) {
+  for (int col = 0; col < 3 && !game_over; col++)
+  {
     int player_one_points = 0;
-    for (int row = 0; row < 3; row++) {
+    for (int row = 0; row < 3; row++)
+    {
       // printf("%d ", col);
       // printf("%d, ", board[row][col]);
-      if (board[row][col] == 1) {
+      if (board[row][col] == 1)
+      {
         player_one_points++;
-      } else if (board[row][col] == 2) {
+      }
+      else if (board[row][col] == 2)
+      {
         player_one_points--;
       }
 
-      if (player_one_points >= 3) {
+      if (player_one_points >= 3)
+      {
         printf("Player 1 wins!\n");
         game_over = 1;
         return 1;
-      } else if (player_one_points <= -3) {
+      }
+      else if (player_one_points <= -3)
+      {
         printf("Player 2 wins!\n");
         game_over = 1;
         return 0;
@@ -78,31 +99,41 @@ int check_board_vertical (int board[3][3])
   return 0;
 }
 
-int check_broad_cross (int board[3][3])
+int check_broad_cross(int board[3][3])
 {
   int player_x_one_points = 0;
   int player_x_r_one_points = 0;
   int ix = 2;
-  for (int i = 0; i < 3; i++) {
-    if (board[i][i] == 1) {
+  for (int i = 0; i < 3; i++)
+  {
+    if (board[i][i] == 1)
+    {
       player_x_one_points++;
-    } else if (board[i][i] == 2) {
+    }
+    else if (board[i][i] == 2)
+    {
       player_x_one_points--;
     }
 
     printf("%d, ", ix);
-    if (board[i][ix] == 1) {
+    if (board[i][ix] == 1)
+    {
       player_x_r_one_points++;
-    } else if (board[i][ix] == 2) {
+    }
+    else if (board[i][ix] == 2)
+    {
       player_x_r_one_points--;
     }
 
     ix--;
 
-    if (player_x_one_points >= 3 || player_x_r_one_points >= 3) {
+    if (player_x_one_points >= 3 || player_x_r_one_points >= 3)
+    {
       printf("Player 1 wins!\n");
       return 1;
-    } else if (player_x_one_points <= -3 || player_x_r_one_points <= -3) {
+    }
+    else if (player_x_one_points <= -3 || player_x_r_one_points <= -3)
+    {
       printf("Player 2 wins!\n");
       return 2;
     }
--- src/main.c
+++ src/main.c
@@ -4,49 +4,59 @@
 
 #include "resource_dir.h"
 
-int main ()
+int main()
 {
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	InitWindow(500, 500, "Tic Tac Toe");
-	SearchAndSetResourceDir("resources");
+  InitWindow(500, 500, "Tic Tac Toe");
+  SearchAndSetResourceDir("resources");
 
-	SetTargetFPS(60);
+  SetTargetFPS(60);
 
-	// Game State
-	int board[3][3] = { 0 };
-	int is_player_turn_one = 1;
+  // Game State
+  int board[3][3] = {0};
+  int is_player_turn_one = 1;
   int player_winner = 0;
   int game_over = 0;
-  
+
   char *message;
   Color color;
 
-	while (!WindowShouldClose())
-	{
-    if (!game_over) {
-      if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+  while (!WindowShouldClose())
+  {
+    if (!game_over)
+    {
+      if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+      {
         Vector2 mousePosition = GetMousePosition();
         int row = mousePosition.y;
         int col = mousePosition.x;
 
-        if (col >= 100 && col <= 400 && row >= 100 && row <= 400) {
+        if (col >= 100 && col <= 400 && row >= 100 && row <= 400)
+        {
           int rowIndex = 2;
           int colIndex = 2;
 
-          if (row < 200) {
+          if (row < 200)
+          {
             rowIndex = 0;
-          } else if (row < 300) {
+          }
+          else if (row < 300)
+          {
             rowIndex = 1;
           };
 
-          if (col < 200) {
+          if (col < 200)
+          {
             colIndex = 0;
-          } else if (col < 300) {
+          }
+          else if (col < 300)
+          {
             colIndex = 1;
           };
 
-          if (board[rowIndex][colIndex] == 0) {
+          if (board[rowIndex][colIndex] == 0)
+          {
             board[rowIndex][colIndex] = (is_player_turn_one) ? 1 : 2;
 
             is_player_turn_one = !is_player_turn_one;
@@ -57,62 +67,76 @@ int main ()
       message = (is_player_turn_one == 1) ? "Player 1's turn" : "Player 2's turn";
       color = (is_player_turn_one == 1) ? RED : GREEN;
 
-      int winner =  check_board_horizontal(board) ||
-                    check_board_vertical(board) ||
-                    check_broad_cross(board);
+      int winner = check_board_horizontal(board) ||
+                   check_board_vertical(board) ||
+                   check_broad_cross(board);
 
-      if (winner) {
+      if (winner)
+      {
         game_over = 1;
         player_winner = winner;
-      } else {
+      }
+      else
+      {
         int is_draw = check_board_draw(board);
-        if (is_draw) {
+        if (is_draw)
+        {
           game_over = 1;
           player_winner = 0;
         }
       }
-    } else {
-      if (IsKeyPressed(KEY_SPACE)) {
+    }
+    else
+    {
+      if (IsKeyPressed(KEY_SPACE))
+      {
         reset_board(board);
         player_winner = 0;
         game_over = 0;
         is_player_turn_one = 1;
       }
 
-      if (player_winner != 0) {
+      if (player_winner != 0)
+      {
         message = (player_winner == 1) ? "Player 1 wins!" : "Player 2 wins!";
         color = (player_winner == 1) ? RED : GREEN;
-      } else {
+      }
+      else
+      {
         message = "Draw game!";
         color = WHITE;
       }
     }
 
-		BeginDrawing();
-		ClearBackground(BLACK);
-		DrawText("Tic Tac Toe", 10, 10, 20, WHITE);
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Tic Tac Toe", 10, 10, 20, WHITE);
 
     // Game Header
     DrawText(message, 180, 50, 20, color);
 
-		// draw the board
-		for (int i = 1; i <= 4; i++) {
-			DrawLine(100, 100 * i, 400, 100 * i, WHITE); // horizontal
-			DrawLine(100 * i, 100, 100 * i, 400, WHITE); // vertical
-		}
+    // draw the board
+    for (int i = 1; i <= 4; i++)
+    {
+      DrawLine(100, 100 * i, 400, 100 * i, WHITE); // horizontal
+      DrawLine(100 * i, 100, 100 * i, 400, WHITE); // vertical
+    }
 
-		for (int row = 0; row < 3; row++) {
-			for (int col = 0; col < 3; col++) {
-				if (board[row][col] != 0) {
-					Color color = (board[row][col] == 1) ? RED : GREEN;
-					DrawCircle(((col + 1) * 100) + 50, ((row + 1) * 100) + 50, 30, color);
-				}
-			}
-		}
-		
-		EndDrawing();
-	}
+    for (int row = 0; row < 3; row++)
+    {
+      for (int col = 0; col < 3; col++)
+      {
+        if (board[row][col] != 0)
+        {
+          Color color = (board[row][col] == 1) ? RED : GREEN;
+          DrawCircle(((col + 1) * 100) + 50, ((row + 1) * 100) + 50, 30, color);
+        }
+      }
+    }
 
-	CloseWindow();
-	return 0;
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
