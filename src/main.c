1ed35386968a202e3e8a90d97de14fac97e3ac8d
Miko Chu <miko.codecrafters@gmail.com>
2024-11-02 13:05:11 +0800

    add simple tic tac toe board and detect mechanism on click

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <stdio.h>
 
 #include "resource_dir.h"
 
@@ -6,16 +7,76 @@ int main ()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(500, 500, "Tic Tac Toe");
 	SearchAndSetResourceDir("resources");
-	
+
+	SetTargetFPS(60);
+
+	// Game State
+	int grid[3][3] = { 0 };
+
+	bool isPlayerOne = true;
+
 	while (!WindowShouldClose())
 	{
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+			Vector2 mousePosition = GetMousePosition();
+			int row = mousePosition.y;
+			int col = mousePosition.x;
+
+			// if (col < 100 || col > 400) {
+			// 	printf("Col out of bound\n");
+			// 	fflush(stdout);
+			// }
+
+			// if (row < 100 | row > 400) {
+			// 	printf("Row out of bound \n");
+			// 	fflush(stdout);
+			// }
+
+			if (col >= 100 && col <= 400 && row >= 100 && row <= 400) {
+				isPlayerOne = !isPlayerOne;
+
+				int rowIndex = 2;
+				int colIndex = 2;
+
+				if (row < 200) {
+					rowIndex = 0;
+				} else if (row < 300) {
+					rowIndex = 1;
+				};
+
+				if (col < 200) {
+					colIndex = 0;
+				} else if (col < 300) {
+					colIndex = 1;
+				};
+
+				printf("rowIndex: %d, colIndex: %d\n", rowIndex, colIndex);
+				fflush(stdout);
+			}
+
+			// printf("row: %d, col: %d\n", row, col);
+			// fflush(stdout);
+		}
+
 		BeginDrawing();
-
 		ClearBackground(BLACK);
+		DrawText("Tic Tac Toe", 10, 10, 20, WHITE);
 
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		DrawText(
+			(isPlayerOne) ? "Player 1" : "Player 2",
+			200,
+			50,
+			20,
+			(isPlayerOne) ? RED : GREEN
+		);
+
+		// draw the grid
+		for (int i = 1; i <= 4; i++) {
+			DrawLine(100, 100 * i, 400, 100 * i, WHITE); // horizontal
+			DrawLine(100 * i, 100, 100 * i, 400, WHITE); // vertical
+		}
 		
 		EndDrawing();
 	}
